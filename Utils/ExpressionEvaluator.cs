6c641e2dad400e47404bc713d670a43cbe4d668a
kli2013 <ayubqb@163.com>
2026-05-17 23:15:42 +0800

    Create ExpressionEvaluator.cs

---
+++ Utils/ExpressionEvaluator.cs
@@ -0,0 +1,14 @@
+using System.Data;
+
+public static class ExpressionEvaluator
+{
+    public static int Evaluate(string expression, int mainW, int mainH, int boxW, int boxH)
+    {
+        string expr = expression.Replace("W", mainW.ToString())
+                                .Replace("H", mainH.ToString())
+                                .Replace("w", boxW.ToString())
+                                .Replace("h", boxH.ToString());
+        var result = new DataTable().Compute(expr, null);
+        return Convert.ToInt32(result);
+    }
+}

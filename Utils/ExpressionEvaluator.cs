b071bfdf9e88e231a74c91065e4554b7caef2d6c
kli2013 <ayubqb@163.com>
2026-05-18 01:13:07 +0800

    Update ExpressionEvaluator.cs

--- Utils/ExpressionEvaluator.cs
+++ Utils/ExpressionEvaluator.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Data;
 
-public static class ExpressionEvaluator
+namespace FFLiteGUI.Utils
 {
-    public static int Evaluate(string expression, int mainW, int mainH, int boxW, int boxH)
+    public static class ExpressionEvaluator
     {
-        string expr = expression.Replace("W", mainW.ToString())
-                                .Replace("H", mainH.ToString())
-                                .Replace("w", boxW.ToString())
-                                .Replace("h", boxH.ToString());
-        var result = new DataTable().Compute(expr, null);
-        return Convert.ToInt32(result);
+        public static int Evaluate(string expression, int mainW, int mainH, int boxW, int boxH)
+        {
+            string expr = expression.Replace("W", mainW.ToString())
+                                    .Replace("H", mainH.ToString())
+                                    .Replace("w", boxW.ToString())
+                                    .Replace("h", boxH.ToString());
+            var result = new DataTable().Compute(expr, null);
+            return Convert.ToInt32(result);
+        }
     }
 }

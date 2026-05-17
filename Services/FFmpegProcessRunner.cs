c5d274471c58d7767ef3b0bba2bbbe0789c4f86f
kli2013 <ayubqb@163.com>
2026-05-17 23:11:11 +0800

    Create FFmpegProcessRunner.cs

---
+++ Services/FFmpegProcessRunner.cs
@@ -0,0 +1,25 @@
+public class FFmpegProcessRunner
+{
+    public async Task<(bool Success, string Error)> RunAsync(string command, CancellationToken token)
+    {
+        var psi = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = $"/c {command}",
+            UseShellExecute = false,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            CreateNoWindow = true
+        };
+        using var process = new Process { StartInfo = psi };
+        var outputBuilder = new StringBuilder();
+        process.OutputDataReceived += (s, e) => { if (e.Data != null) outputBuilder.AppendLine(e.Data); };
+        process.ErrorDataReceived += (s, e) => { if (e.Data != null) outputBuilder.AppendLine(e.Data); };
+
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        await process.WaitForExitAsync(token);
+        return (process.ExitCode == 0, outputBuilder.ToString());
+    }
+}
